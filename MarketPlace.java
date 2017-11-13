c71729b3b648f6116d5704996a1e052ead051239
chandlerkeyes <ckeyes4@emich.edu>
2017-11-13 15:57:59 -0500

    Instantiated MarketPlace instance variables

--- MarketPlace.java
+++ MarketPlace.java
@@ -1,6 +1,30 @@
+import java.util.ArrayList;
+import java.util.PriorityQueue;
+
 public class MarketPlace {
+    private final int OPEN = 600;
+    private final int CLOSE = 1080;
     private double avgTimeBtwnCustomers;
     private double avgCashierServTime;
     private int numOfCashiers;
     private boolean displayCheckout;
+    private GVrandom arrivalAndServTimes;
+    private double currentSimTime;
+    private Customer[] cashier;
+    private ArrayList <Customer> customersInLine;
+    private PriorityQueue<GVevent> futureEvents;
+    private String results;
+
+    public MarketPlace() {
+        customersInLine = new ArrayList<Customer>();
+        futureEvents = new PriorityQueue <GVevent> ();
+        cashier = new Customer[numOfCashiers];
+        arrivalAndServTimes = new GVrandom();
+        numOfCashiers = 3;
+        avgCashierServTime = 6.6;
+        //avg arrival time
+        avgTimeBtwnCustomers = 2.5;
+    }
+    
+
 }
